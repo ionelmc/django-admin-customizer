4de66240f2e39870b4930af65d76484218f3815e
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-06-01 23:21:00 +0300

    Update test settings.

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name = "django-admin-customizer",
-    version = "0.2",
-    url = 'https://github.com/ionelmc/django-admin-customizer',
-    download_url = '',
-    license = 'BSD',
-    description = "Django admin customizing interface",
-    long_description = file(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
-    author = 'Ionel Cristian Mărieș',
-    author_email = 'contact@ionelmc.ro',
-    packages = find_packages('src'),
-    package_dir = {'':'src'},
-    include_package_data = True,
-    install_requires = [
+    name="django-admin-customizer",
+    version="0.2",
+    url='https://github.com/ionelmc/django-admin-customizer',
+    download_url='',
+    license='BSD',
+    description="Django admin customizing interface",
+    long_description=file(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Ionel Cristian Mărieș',
+    author_email='contact@ionelmc.ro',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
         'Django',
     ],
-    zip_safe = False,
-    classifiers = [
+    zip_safe=False,
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
